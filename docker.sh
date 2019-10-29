87eae269470e4addac7cacf3ab89d7892dba1e7c
Cplus Shen <cplus.shen@gmail.com>
2019-10-29 06:44:40 +0000

    apt install docker-cm without asking

--- docker.sh
+++ docker.sh
@@ -59,7 +59,7 @@ case "$1" in
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
     sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
     sudo apt-get update
-    sudo apt-get install docker-ce
+    sudo apt-get install -y docker-ce
     ;;
 
     clean)
