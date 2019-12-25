4297b34205ceb9d4ef96cafdb651119b359734a7
Cplus Shen <cplus.shen@gmail.com>
2019-12-25 01:37:18 +0000

    added Ubuntu 16.04 docker support

--- docker.sh
+++ docker.sh
@@ -35,6 +35,14 @@ case "$1" in
     docker run -d -P --name $2 cplus/openssh-server:0.1
     ;;
 
+    ssh-u16)
+    if [ $# -ne 3 ]; then
+      echo "Usage: $0 ssh-u16 <CONTAINER_NAME> <SSH_PORT>"
+      exit 1
+    fi
+    docker run -d -P --name $2 -p $3:22 cplus/openssh-server-ubuntu-16:0.1
+    ;;
+
     exec)
     docker exec -ti $2 /bin/bash
     ;;
@@ -68,7 +76,11 @@ case "$1" in
     sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
     sudo apt-get update
     sudo apt-get install -y docker-ce
+    sudo newgrp - docker
     sudo usermod -aG docker $USER
+    sudo usermod -aG docker root
+    sudo gpasswd -a $USER docker
+    sudo gpasswd -a root docker
     ;;
 
     clean)
@@ -85,8 +97,12 @@ case "$1" in
     docker pull ubuntu:14.04
     ;;
 
+    build-image)
+    docker build -t $2 .
+    ;;
+
     *)
-    echo -e "Usage: $0 { create | exec | start | stop | rm } CONTAIN_NAME\n\
+    echo -e "Usage: $0 { create | exec | start | stop | attach | rm } CONTAIN_NAME\n\
        $0 status\n\
        $0 install\n\
        $0 clean\n\
